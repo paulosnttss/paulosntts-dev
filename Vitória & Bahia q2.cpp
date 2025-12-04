0ed3053714239c4c58e5841ccf7e6ca268651a50
Pauloss santos <snttsspaulo@gmail.com>
2025-12-04 11:08:32 -0300

    Add files via upload

---
+++ Vitória & Bahia q2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int contador = 10;
+
+    while (contador >= 0) {
+        printf("%d\n", contador);
+        contador--;
+    }
+
+    printf("FIM! A bola vai rolar no clássico Ba-Vi!\n");
+
+    return 0;
+}
+
