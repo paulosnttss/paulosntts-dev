9a122804c8a00128e7fe1c712fdf7178d5264bb1
Pauloss santos <snttsspaulo@gmail.com>
2025-12-04 11:06:48 -0300

    Add files via upload

---
+++ Estrutura de Repetição goku q2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int energia = 10;
+
+    while (energia >= 0) {
+        printf("%d\n", energia);
+        energia--;
+    }
+
+    printf("FIM!\n");
+
+    return 0;
+}
+
